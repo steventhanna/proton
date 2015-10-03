f43843ffaa6e3711b5674a9cd06c96ab6e4233c0
steventhanna <steventhanna@gmail.com>
2015-10-03 03:00:04 -0400

    Begin open file handle

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var app = require('app'); // Module to control application life.
 var BrowserWindow = require('browser-window'); // Module to create native browser window.
 var Menu = require('menu');
 var MenuItem = require('menu-item');
+// var ipc = require('ipc');
+// var remote = require('remote');
+var dialog = require('dialog');
 
 var menu = new Menu();
 
@@ -24,22 +27,22 @@ app.on('window-all-closed', function() {
 });
 
 // Create the menubar
-var template = [{
-  label: 'File',
-  submenu: [{
-    label: 'New',
-    accelerator: 'CmdOrCtrl+N',
-    role: 'new',
-  }, {
-    label: 'Save',
-    accelerator: 'CmdOrCtrl+S',
-    role: 'save',
-  }, {
-    label: 'Save As',
-    accelerator: 'CmdOrCtrlSh+Shift+S',
-    role: 'saveAs',
-  }, ]
-}, ];
+// var template = [{
+//   label: 'File',
+//   submenu: [{
+//     label: 'New',
+//     accelerator: 'CmdOrCtrl+N',
+//     role: 'new',
+// }, {
+//   label: 'Save',
+//   accelerator: 'CmdOrCtrl+S',
+//   role: 'save',
+// }, {
+//   label: 'Save As',
+//   accelerator: 'CmdOrCtrlSh+Shift+S',
+//   role: 'saveAs',
+// }, ]
+// }, ];
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -47,7 +50,7 @@ app.on('ready', function() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
   });
 
   // Make the window fill the screen
@@ -59,9 +62,118 @@ app.on('ready', function() {
   // Open the DevTools.
   mainWindow.openDevTools();
 
-  // Init the menu
-  menu = Menu.buildFromTemplate(template);
-  Menu.setApplicationMenu(menu);
+  var appmenu_template = [{
+    label: 'Proton',
+    submenu: [{
+      label: 'About Proton',
+      click: function() {
+        ipc.send('open-url-in-external', 'http://github/steventhanna/proton/')
+      }
+    }, {
+      type: 'separator'
+    }, {
+      label: 'Quit',
+      accelerator: 'Command+Q',
+      click: function() {
+        ipc.send('close')
+      }
+    }]
+  }, {
+    label: 'File',
+    submenu: [{
+      label: 'Open File',
+      accelerator: 'CmdOrCtrl+O',
+      click: function() {
+        // ipc.send('open-file-dialog')
+        var filename = dialog.showOpenDialog({
+          properties: ['openFile'],
+          // restrict to markdown files only
+          filters: [{
+            name: 'Markdown',
+            extensions: ['md']
+          }],
+        });
+        // What do you know it works.
+        console.log(filename);
+      }
+    }, {
+      label: 'New File',
+      accelerator: 'CmdOrCtrl+N',
+      click: function() {
+        console.log("NEW FILE CLICKED");
+      }
+    }, {
+      label: 'Save',
+      accelerator: 'CmdOrCtrl+S',
+      click: function() {
+        console.log("SAVE FILE CLICKED");
+      },
+    }]
+  }, {
+    label: 'Edit',
+    submenu: [{
+      label: 'Undo',
+      accelerator: 'CmdOrCtrl+Z',
+      role: 'undo'
+    }, {
+      label: 'Redo',
+      accelerator: 'Shift+CmdOrCtrl+Z',
+      role: 'redo'
+    }, {
+      type: 'separator'
+    }, {
+      label: 'Cut',
+      accelerator: 'CmdOrCtrl+X',
+      role: 'cut'
+    }, {
+      label: 'Copy',
+      accelerator: 'CmdOrCtrl+C',
+      role: 'copy'
+    }, {
+      label: 'Paste',
+      accelerator: 'CmdOrCtrl+V',
+      role: 'paste'
+    }, {
+      label: 'Select All',
+      accelerator: 'CmdOrCtrl+A',
+      role: 'selectall'
+    }, ]
+  }, {
+    label: 'Window',
+    submenu: [{
+      label: 'Minimize',
+      accelerator: 'Command+M',
+      click: function() {
+        ipc.send('minimize')
+      }
+    }, {
+      label: 'Toggle Full Screen',
+      accelerator: 'Command+Enter',
+      // click: onfullscreentoggle
+    }]
+  }, {
+    label: 'Help',
+    submenu: [{
+      label: 'Report Issue',
+      click: function() {
+        ipc.send('open-url-in-external', 'https://github.com/mafintosh/playback/issues')
+      }
+    }, {
+      label: 'View Source Code on GitHub',
+      click: function() {
+        ipc.send('open-url-in-external', 'https://github.com/mafintosh/playback')
+      }
+    }, {
+      type: 'separator'
+    }, {
+      label: 'Releases',
+      click: function() {
+        ipc.send('open-url-in-external', 'https://github.com/mafintosh/playback/releases')
+      }
+    }]
+  }]
+  var appmenu = Menu.buildFromTemplate(appmenu_template)
+  Menu.setApplicationMenu(appmenu)
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {

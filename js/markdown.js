cbbd001513c6ae6f50525e7278bdae93cdaf86ee
steventhanna <steventhanna@gmail.com>
2015-10-20 11:30:32 -0400

    Having some problems with JAX.... Going to have to download the actual files, and not use the CDN

--- js/markdown.js
+++ js/markdown.js
@@ -1,4 +1,5 @@
 var ipc = require('ipc');
+var marked = require('marked');
 
 ipc.on('fileContent', fileData => {
   console.log("GOT FILE CONTENT");
@@ -7,7 +8,40 @@ ipc.on('fileContent', fileData => {
   var editor = ace.edit("editor");
   editor.setValue(fileData);
   ipc.on('extension', extension => {
-    editor.getSession().setMode("ace/mode/tex");
+    if (extension == "tex") {
+      // Iniital Render
+      editor.getSession().setMode("ace/mode/tex");
+      var latex = editor.getValue();
+      var result = document.getElementById("previewText");
+      typejax.updater.init(latex, latex.length, result);
+      // Continous render
+      editor.getSession().on('change', function(e) {
+        var latex = editor.getValue();
+        var result = document.getElementById("previewText");
+        typejax.updater.init(latex, latex.length, result);
+      });
+    } else {
+      editor.getSession().setMode("ace/mode/markdown");
+      marked.setOptions({
+        renderer: new marked.Renderer(),
+        gfm: true,
+        tables: true,
+        breaks: true,
+        pedantic: true,
+        sanitize: true,
+        smartLists: true,
+        smartypants: true
+      });
+      // First initial render
+      var text = editor.getValue();
+      document.getElementById('previewText').innerHTML = marked(text);
+      // Continous render
+      editor.getSession().on('change', function(e) {
+        // e.type, etc
+        var text = editor.getValue();
+        document.getElementById('previewText').innerHTML = marked(text);
+      });
+    }
   });
 });
 

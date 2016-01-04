0976803bc690c5bc0c91a3b78ed3cf971c2e680e
steventhanna <steventhanna@gmail.com>
2016-01-04 16:26:54 -0600

    Made a psuedo sticky elert bar using a bootstrap stick navbar, closes #25

--- js/index.js
+++ js/index.js
@@ -46,13 +46,13 @@ ipc.on('getSaveAs', fileData => {
 ipc.on('error', errorMessage => {
   var type = errorMessage.type;
   var message = errorMessage.message;
-  document.getElementById('error').innerHTML = '<div style="margin-right: 10px;" class="alert alert-' + type + ' alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + message + '</div>';
+  document.getElementById('error').innerHTML = '<div style="margin-right: 10px; position: relative;" class="alert alert-' + type + ' alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + message + '</div>';
   window.setTimeout(function() {
     var alertT = '.alert-' + type;
     // TODO :: Implement JQuery fade out of alert.
     // $("#error").fadeOut();
     $(alertT).alert('close');
-  }, 2000);
+  }, 3000);
 });
 
 
